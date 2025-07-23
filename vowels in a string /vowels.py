027e4ab90903dc62e0e4f4e3f8a94ed0349c3f9b
Sivanesan L <sivanesanecetech@gmail.com>
2025-07-23 21:56:56 +0530

    Delete vowels in a string  directory

--- vowels in a string /vowels.py
+++
@@ -1,9 +0,0 @@
-import time 
-name=str(input("Enter the string :\n"))
-time.sleep(2)
-def vowels(name):
-    name=name.lower()
-    for i in name:
-       if(i in 'aeiou'):
-         print(i)
-vowels(name)
